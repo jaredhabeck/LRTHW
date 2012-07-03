9f8d70fde7d63e99c376e7e3c95b3faba794dc72
Jared Habeck <jhabeck@gmail.com>
2012-07-03 10:44:38 -0400

    finished 26, made 28 into a test function for quizzing!

---
+++ ex28.rb
@@ -0,0 +1,39 @@
+test = {
+  "true and true" => true,
+  "false and true" => false,
+  "1 == 1 and 2 == 1" => false,
+  "'test' == 'test'" => true,
+  "1 == 1 or 2 != 1" => true, 
+  "true and 1 == 1" => true,
+  "false and 0 != 0" => false,
+  "true or 1 == 1" => true,
+  "'test' == 'testing'" => false,
+  "1 != 0 and 2 == 1" => false,
+  "'test' != 'testing'" => true,
+  "'test' == 1" => false,
+  "not (true and false)" => true,
+  "not (1 == 1 and 0 != 1)" => true,
+  "not (10 == 1 or 1000 == 1000)" => false,
+  "not ('testing' == 'testing' and 'Jared' == 'Cool Guy')" => false,
+  "1 == 1 and not ('testing' == 1 or 1 == 0)" => true,
+  "'chunky' == 'bacon' and not (3 == 4 or 3 == 3)" => false,
+  "3 == 3 and not ('testing' == 'testing' or 'Ruby' == 'Fun')" => false
+}
+
+score = 0
+count = 1
+total = test.size
+test.each do |question, solution|
+  puts "#{count}) #{question}"
+  print '?> '
+  answer = STDIN.gets.chomp
+  answer = answer == "true" ? true : false;
+  if (answer == solution)
+    puts 'correct!'
+    score += 1
+  else
+    puts 'nope!'
+  end
+  count += 1
+  puts "Your score is #{score}/#{total}!";
+end
